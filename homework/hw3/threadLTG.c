e182beca3ce1fcb2bb08a2c1e827e2e0a92ef374
lgrube <lgv9483@saintjoe.edu>
2015-10-13 10:33:10 -0500

    working correctly

--- homework/hw3/threadLTG.c
+++ homework/hw3/threadLTG.c
@@ -6,20 +6,23 @@
 #include <pthread.h>
 #include <sys/types.h>
 #include <sys/syscall.h>
-//#include "lsLinkFile"
+#include <stdlib.h>
 
 void* helloThread(void* tmp) {
 printf("Hello world, I am thread: %u\n", (unsigned int)pthread_self());
-sleep(2); // Keep it alive so we're sure the second thread gets a unique ID.
+sleep(2);
 return NULL;
 }
 
+int system (const char *command);
+
 int main(int argc, char **argv) {
 pthread_t thread1, thread2;
-sleep(1); // Hack to avoid race for stdout.
+sleep(1);
 pthread_create(&thread2, NULL, helloThread, NULL);
 pthread_join(thread1, NULL);
 pthread_join(thread2, NULL);
+system("ls");
 printf("I am the father process: %d\n", getpid());
 return(0);
 }
