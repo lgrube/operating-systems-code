537d6a569d28fc4fa257cc05dea6fdcd6ca254be
lgrube <lgv9483@saintjoe.edu>
2015-11-04 12:58:31 -0600

    homework 5 added and everything else

--- homework/hw5/RR_printer.c
+++ homework/hw5/RR_printer.c
@@ -1,11 +1,19 @@
 #include <stdio.h>
 int main(){
-    int m,n;
+  int m,n;
+  printf("Please enter number of threads: ");
+  scanf("%d", &n);
+  printf("Please enter number to print out: ");
+  scanf("%d", &m);
 
-
-
-
-    
-
+//    int i = 1; //this is n
+//    int j = 4; //this is m
+    int k = m; //this is a test variable
+    while(n<=k) {
+        for (n=n; n<=k; n++) {
+          printf("thread number: %d has printed out: %d \n", n , m);
+          m++;
+          }
+    }
     return 0;
 }
