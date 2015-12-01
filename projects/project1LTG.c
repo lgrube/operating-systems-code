3060ff487f08bbf283e5c7250b2b26576d3eb139
lgrube <lgv9483@saintjoe.edu>
2015-12-01 08:21:01 -0600

    have both sections working going to be turning it in

--- projects/project1LTG.c
+++ projects/project1LTG.c
@@ -1,21 +1,28 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-char name[20] = ""; //used for name of student
-int compsci = 0; //used for computer science score
-int cal = 0;//used for calculus score
-int writing = 0;//used for writing score
-int volleyball = 0;//used for volleyball score
-int total = 0; //used to add all the scores up
-int numOfRuns = 0; //number of students entering in
+//luke grube
+//december 1, 2015
+//everything in big comment sections are debugging info
 
-int main() {
+int main(void) {
+	//declaring all the variables
+	int a[500], n, compsci = 0, cal = 0, writing = 0, volleyball = 0, total = 0, temp=0;
+	int MAXLENGTH = 25; //sets maxlength of name
 
   printf("how many students are you entering in: ");
-  scanf("%d", &numOfRuns);
+  scanf("%d", &n);
+  printf ("\n");
 
-  for(int i=0; i<numOfRuns; i++) {
+	int MAXSTRING = n;
+	char firstname[MAXSTRING][MAXLENGTH];
+//	printf("MAXSTRING %d\n", MAXSTRING);
+
+/* this section ask for the name and each score then totals it*/
+	for (int i = 0; i <n; i++)
+	{
     printf("what was the students name: ");
-    scanf("%s", &name);
+    scanf("%s", firstname[i]);
     printf("what was the grade for computer science: ");
     scanf("%d", &compsci);
     printf("what was the grade for calculus: ");
@@ -24,19 +31,47 @@ int main() {
     scanf("%d", &writing);
     printf("what was the grade for volleyball: ");
     scanf("%d", &volleyball);
-
+		printf("\n");
     total = compsci + cal + writing + volleyball;
-//    printf("total test: %d", total);
+    a[i] = total;
 
-    printf("Students name: %s \n", name);
-    printf("Students grade for computer science: %d \n", compsci);
-    printf("Students grade for calculus: %d \n", cal);
-    printf("Students grade for writing: %d \n", writing);
-    printf("Students grade for volleyball: %d \n", volleyball);
-    printf("Students total grade: %d \n", total);
+//		b[i+1] = total;
+//    printf("this is i %d\n", i);
+//    printf("total test: %d \n", total);
+//    printf("this is a[i] %d \n", a[i]);
+//		printf("this is b[i] %d\n", b[i-1]);
+
+  }
+
+//  for(int i=0; i<MAXSTRING; i++) {
+//    printf("this is a name %s\n", firstname[i]);
+//		fgets(firstname[i], MAXLENGTH, stdin);
+//  }
+//  for(int i=0; i<n; i++){
+//    printf("This student %s", firstname[i]);
+//		printf(" this is their total score %d\n", a[i]);
+//  }
 
 
-//    printf("ending %d; number of runs %d", compsci, numOfRuns);
-  } // end of for
+/* this is what does the organization*/
+	for (int i=0 ; i<n ; i++) {
+//		for (int j=0; j<n-i; j++) {
+			if(a[i] < a[i+1]) {
+				temp = a[i + 1];
+				a[i + 1] = a[i];
+				a[i] = temp;
+//				printf("this is temp %d\n", temp);
+//			printf("this is sub b %d\n", b[i]);
+			}
+//		printf (" this is sub a %d", a[i]);
+//		}
+	}
+	/* this sections is what prints it all our in order*/
+	for(int i=0; i<n; i++){
+		printf("%s", firstname[i]);
+		printf(" %d\n", a[i]);
+	}
+	printf("\n");
 
-} // end of main
+  return 0;
+}
